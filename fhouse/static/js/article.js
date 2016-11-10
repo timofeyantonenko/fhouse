5eaa8f9954cc927383ca6f522d9f5825614ff39d
mangura dmitriy <mangura.9@yandex.ru>
2016-11-10 20:21:23 +0200

    greate main page and article, fixed bags in records

--- fhouse/static/js/article.js
+++ fhouse/static/js/article.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-    $('.one_news_img img').each(function(i) {
+    $(".one_read_article:nth-child(3n+1)").css("margin-left", "0");
+
+    $(".nav_ul li:first-child a").addClass("active_nav_ul");
+
+    $(".nav_ul li a").click(function(e) {
+        e.preventDefault();
+        $(".nav_ul li a").removeClass('active_nav_ul');
+        $(this).addClass('active_nav_ul');
+    });
+
+    $('.one_read_article_img img').each(function(i) {
         if ($(this).css("height") < $(this).parent().css("height")) {
             $(this).css({ "height": "100%", "width": "auto", "align-self": "center" });
             $(this).parent().css({ "display": "flex", "justify-content": "center" });
         }
     });
-
-    $('one_news_img img').each(function(i) {
+    $('.one_read_article_img img').each(function(i) {
         if ($(this).css("width") < $(this).parent().css("width")) {
             $(this).css({ "width": "100%", "align-self": "center" });
             $(this).parent().css({ "display": "block", });
         }
     });
-
 });
