ca6deeff9a3d8736decd74fcbf8310ba67d47a1f
mangura dmitriy <mangura.9@yandex.ru>
2016-11-25 17:24:45 +0200

    minor changes in gallery all file

--- fhouse/static/js/gallery.js
+++ fhouse/static/js/gallery.js
@@ -12,37 +12,34 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
+
 function isEmpty(obj) {
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop))
             return false;
     }
 
     return true;
 }
 if (!String.format) {
-  String.format = function(format) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return format.replace(/{(\d+)}/g, function(match, number) {
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
+    String.format = function(format) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        return format.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
 }
-$(document).ready(function(){
+$(document).ready(function() {
     var section = getUrlParameter('section');
-    if (typeof section != 'undefined'){
+    if (typeof section != 'undefined') {
         var search_tab = (tab == 'all' || typeof tab == 'undefined') ? 'Все' : tab;
         var previous_active_tab = $('div .menu_individualNews_active');
         previous_active_tab.removeClass('menu_individualNews_active');
         var str_to_find = String.format("div.changeNews:contains('{0}')", search_tab);
         var new_active_tab = $(str_to_find);
         new_active_tab.addClass('menu_individualNews_active');
-    }
-    else {
-        section =  $(".nav_ul li:first-child a");
+    } else {
+        section = $(".nav_ul li:first-child a");
         section_name = section.text();
         console.log(section_name);
         // Active section of menu
@@ -54,103 +51,221 @@ $(document).ready(function(){
             url: ajax_url,
             data: ajax_data,
             dataType: "html",
-            success: function(data){
+            success: function(data) {
                 var content = $('.slider_photo_list');
                 content.html(data);
-//                window.history.pushState("object or string", "Title", state);
+                //                window.history.pushState("object or string", "Title", state);
             },
-            error: function(xhr, status, error){
+            error: function(xhr, status, error) {
                 console.log(error, status, xhr);
             }
         });
     }
 
-// Активный раздел меню
-//$(".nav_ul li:first-child a").addClass("active_nav_ul");
+    // Активный раздел меню
+    //$(".nav_ul li:first-child a").addClass("active_nav_ul");
 
-$(".nav_ul li a").click(function(e) {
-    if($(this).hasClass('active_nav_ul')){
-        return;
-    }
-    e.preventDefault();
-    $(".nav_ul li a").removeClass('active_nav_ul');
-    section =  $(this);
-    section_name = section.text().trim();
-    // Active section of menu
-    section.addClass("active_nav_ul");
-    var ajax_url = 'slider_photo_list';
-    var ajax_data = {"section": section_name}
-    var state = ""
-    $.ajax({
-        url: ajax_url,
-        data: ajax_data,
-        dataType: "html",
-        success: function(data){
-            var content = $('.slider_photo_list');
-            content.html(data);
-//            window.history.pushState("object or string", "Title", state);
-        },
-        error: function(xhr, status, error){
-            console.log(error, status, xhr);
+    $(".nav_ul li a").click(function(e) {
+        if ($(this).hasClass('active_nav_ul')) {
+            return;
         }
+        e.preventDefault();
+        $(".nav_ul li a").removeClass('active_nav_ul');
+        section = $(this);
+        section_name = section.text().trim();
+        // Active section of menu
+        section.addClass("active_nav_ul");
+        var ajax_url = 'slider_photo_list';
+        var ajax_data = { "section": section_name }
+        var state = ""
+        $.ajax({
+            url: ajax_url,
+            data: ajax_data,
+            dataType: "html",
+            success: function(data) {
+                var content = $('.slider_photo_list');
+                content.html(data);
+                //            window.history.pushState("object or string", "Title", state);
+            },
+            error: function(xhr, status, error) {
+                console.log(error, status, xhr);
+            }
+        });
     });
-});
+
+    // Новый слайдер 
 
 
-$(document).on('click', '.foto_left', function(){
-//$('.foto_left').click(function() {
-    $('.left_right_foto').animate({ left: '+=798.656' });
-});
+    // Конец кода для нового слайдера
 
-$(document).on('click', '.foto_right', function(){
-//$('.foto_right').click(function() {
-    console.log('RIIIGHT');
-    $('.left_right_foto').css('left', '-=798.656');
-});
+    $(".nav_ul").click(function() {
+        $('.img_slaider img').each(function(i) {
+            if ($(this).is(":visible")) {
+                // $(this).hide();
+                // $(this).next().show();
+                console.log(this.width());
+            };
+        });
 
-
-if ($(".left_right_foto:nth-child(1)").css("left") == "0px") {
-    $(".foto_left").hide();
-} else {
-
-    $(".foto_left").show();
-};
-
-$(document).on('click', '.foto_right', function(){
-//$(".foto_right").on('click', function() {
-    console.log('RIIIGHT2');
-    $(".foto_left").show();
-});
-
-$(document).on('click', '.foto_left', function(){
-//$(".foto_left").on('click', function() {
-    $(".foto_right").show();
-});
-
-$(document).on('click', '.foto_left', function(){
-//$(".foto_left").on('click', function() {
-	if($(".album_foto .left_right_foto:nth-child(2)").css("left") == "0px") {
-		$(".foto_left").hide();
-	} else $(".foto_left").show();
-});
-
-$(document).on('click', '.foto_right', function(){
-//$(".foto_right").on('click', function() {
-
-    console.log('RIIIGHT3');
-	if($(".album_foto .left_right_foto:nth-last-child(2)").css("left") == "0px") {
-		$(".foto_right").hide();
-	} else $(".foto_right").show();
-});
+    });
 
 
 
+    $(window).load(function() {
 
-$('.main_foto_albub img').each(function(i) {
-    if ($(this).css("height") < $(this).parent().css("height")) {
-        $(this).css({ "height": "100%", "width": "auto", "align-self": "center" });
-        $(this).parent().css({ "display": "flex", "justify-content": "center" });
-    }
-});
+        // Фиксация блока меньшей высоты
+
+        var height = $(".colum:first-child").height();
+        var biggest_col;
+        $(".colum").each(function(i) {
+
+            if ($(this).height() < height) {
+                height = $(this).height()
+            } else {
+                biggest_col = $(this);
+            };
+
+        });
+
+        var min_height_block;
+
+        $(".colum").each(function(i) {
+
+            if ($(this).height() == height) {
+                min_height_block = $(this);
+            };
+
+        });
+
+        console.log(min_height_block.height());
+
+        var width_min_height_block = $(min_height_block).width();
+
+        $(min_height_block).css("width", width_min_height_block);
+
+        var left_col = $(min_height_block).offset().left;
+
+        if (min_height_block.height() + 70 < document.documentElement.clientHeight) {
+            $(min_height_block).addClass("fixed_col_top");
+            $(min_height_block).css("bottom", (document.documentElement.clientHeight - min_height_block.height() - 70));
+            $(min_height_block).css("left", left_col);
+
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > ((document.documentElement.clientHeight - (min_height_block.height() + 50)) + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height())) {
+                    var bottom_scroll = ($(this).scrollTop() - (20 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height()));
+                    $('.fixed_col_top').css('top', 'inherit');
+                    $(min_height_block).css("bottom", bottom_scroll);
+
+
+                } else {
+                    $('.fixed_col_top').css('top', '70px');
+                    $('.fixed_col_top').css('bottom', 'inherit');
+                };
+            })
+
+        } else {
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > (45 + height - document.documentElement.clientHeight + $(".navbar").height()) && $(this).scrollTop() <= (50 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height())) {
+                    $(min_height_block).addClass("fixed_col");
+                    $(min_height_block).css("left", left_col);
+                    $(min_height_block).css("bottom", "25px")
+
+                } else if ($(this).scrollTop() > (45 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height())) {
+                    var bottom_scroll = ($(this).scrollTop() - (20 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height()));
+                    $(min_height_block).css("bottom", bottom_scroll);
+                } else {
+
+
+                    $(min_height_block).removeClass("fixed_col");
+                    $(min_height_block).css({ "left": "0px", "bottom": "0px" });
+                }
+            });
+        }
+
+        // Конец фиксации
+
+        $(".img_slaider img:first-child").show();
+
+        $('.img_slaider img').each(function(i) {
+            var height_slaider = $(".img_slaider").height();
+            var width_slaider = $(".img_slaider").width();
+            var relationship_width_height = (width_slaider / height_slaider);
+            var width_img = $(this).width();
+            var height_img = $(this).height();
+            var relationship_foto = (width_img / height_img);
+            if (relationship_foto > relationship_width_height) {}
+
+            if ((width_img / height_img) > relationship_width_height) {
+                $(this).css({ "width": "100%", "height": "auto" });
+            } else if ((width_img / height_img) < relationship_width_height) {
+                $(this).css({ "width": "auto", "height": "100%" });
+            } else if ((width_img / height_img) == relationship_width_height) {
+                $(this).css({ "width": "100%", "height": "100%" });
+            };
+
+        });
+
+        if ($('.img_slaider img:first-child').is(":visible")) {
+            $(".foto_left").hide();
+        } else {
+            $(".foto_left").show();
+        };
+
+        if ($('.img_slaider img:last-child').is(":visible")) {
+            $(".foto_left").hide();
+        } else {
+            $(".foto_left").show();
+        };
+
+        $(document).ajaxComplete(function() {
+            $(".img_slaider img:first-child").show();
+
+            $('.img_slaider img').each(function(i) {
+                var height_slaider = $(".img_slaider").height();
+                var width_slaider = $(".img_slaider").width();
+                var relationship_width_height = (width_slaider / height_slaider);
+                var width_img = $(this).width();
+                var height_img = $(this).height();
+                var relationship_foto = (width_img / height_img);
+
+                if ((width_img / height_img) > relationship_width_height) {
+                    $(this).css({ "width": "100%", "height": "auto" });
+                } else if ((width_img / height_img) < relationship_width_height) {
+                    $(this).css({ "width": "auto", "height": "100%" });
+                } else if ((width_img / height_img) == relationship_width_height) {
+                    $(this).css({ "width": "100%", "height": "100%" });
+                };
+            });
+
+            if ($('.img_slaider img:first-child').is(":visible")) {
+                $(".foto_left").hide();
+            } else {
+                $(".foto_left").show();
+            };
+
+            if ($('.img_slaider img:last-child').is(":visible")) {
+                $(".foto_left").hide();
+            } else {
+                $(".foto_left").show();
+            };
+
+
+
+        });
+    });
+
+
+
+
+
+
+
+    $(".foto_right").on("click", function() {
+
+        // if ($(".img_slaider img").is(":visible")) {
+        $('.img_slaider img').hide();
+        // $(this).next().show();
+        // }
+    });
 
 });
