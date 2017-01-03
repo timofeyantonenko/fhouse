262a6d35158b26daaa4713a11a0ff21e1f03b475
mangura dmitriy <mangura.9@yandex.ru>
2017-01-03 12:37:26 +0200

    bet doing

--- fhouse/static/js/bet.js
+++ fhouse/static/js/bet.js
@@ -14,6 +14,14 @@ $(document).ready(function() {
         })
     });
 
+    // Выбор топ матча 
+    $(".li_img__top_match").click(function() {
+        $(".li_img__top_match").children("figure").hide();
+        $(".li_img__top_match").children(".team_nav, .preview_math_nav_background").show();
+        $(this).children("figure").show();
+        $(this).children(".team_nav, .preview_math_nav_background").hide();
+    });
+
     $(function() {
         $('#type_comment').on('keyup paste', function() {
 
@@ -23,7 +31,7 @@ $(document).ready(function() {
             if ($el.innerHeight < this.scrollHeight) {
                 $el.height(this.scrollHeight - offset);
             } else {
-                console.log($(this).height());
+
                 $el.height(1);
                 $el.height(this.scrollHeight - offset);
             }
@@ -33,7 +41,7 @@ $(document).ready(function() {
                 var height_add_com = $(".add_comment_prev").height()
                 var height_parent_all_comments = $(".block_comments_for_one_champ").height();
                 $(".block_comments_preview").css({
-                    "height": (height_parent_all_comments - height_add_com - height_title_comment - 20)
+                    "height": (height_parent_all_comments - height_add_com - height_title_comment - 30)
                 })
             });
 
@@ -65,83 +73,6 @@ $(document).ready(function() {
 
 
 
-    $(".navigation_between_preview .nav_ul li:first-child a").addClass("active_nav_ul");
-
-    $(".navigation_between_preview .nav_ul li a").click(function(e) {
-        e.preventDefault();
-        $(".navigation_between_preview .nav_ul li a").removeClass('active_nav_ul');
-        $(this).addClass('active_nav_ul');
-    });
-
-    $(".match_stats li:first-child a").addClass("active_stat");
-
-    $(".match_stats li a").click(function(e) {
-        e.preventDefault();
-        $(".match_stats li a").removeClass('active_stat');
-        $(this).addClass('active_stat');
-    });
-
-    // Навигация между топ-прогнозистами
-    $(".table_nav_forecasters .tab ul li:first-child").addClass("tab_active");
-
-    $(".table_nav_forecasters .tab ul li").click(function(e) {
-        e.preventDefault();
-        $(".table_nav_forecasters .tab ul li").removeClass('tab_active');
-        $(this).addClass('tab_active');
-    });
-
-    // Навигация по прогнозам
-    $(".table_nav_prognossis .tab ul li:first-child").addClass("tab_active");
-
-    $(".table_nav_prognossis .tab ul li").click(function(e) {
-        e.preventDefault();
-        $(".table_nav_prognossis .tab ul li").removeClass('tab_active');
-        $(this).addClass('tab_active');
-    });
-
-    // Выбор результата 
-
-    $(".choose_result > div").click(function(e) {
-        e.preventDefault();
-        $(this).parent().find("div").removeClass('choose_result_active');
-        $(this).addClass('choose_result_active');
-    });
-
-
-
-
-    // Draw, victory or defeat
-
-    $('.last_result tr td:nth-child(3)').each(function(i) {
-        if ($(this).hasClass("this_club")) {
-            $(this).parent().find("td:nth-child(5) span:nth-child(1)").addClass("this_club_result");
-            $(this).parent().find("td:nth-child(5) span:nth-child(2)").addClass("competitor_result");
-        } else {
-            $(this).parent().find("td:nth-child(5) span:nth-child(2)").addClass("this_club_result");
-            $(this).parent().find("td:nth-child(5) span:nth-child(1)").addClass("competitor_result");
-        }
-    });
-
-
-    $('.last_result tr td:nth-child(5)').each(function(i) {
-        var this_club_result = $(this).children(".this_club_result").text();
-        var competitor_result = $(this).children(".competitor_result").text();
-        if (Number(this_club_result) > Number(competitor_result)) {
-            $(this).parent().find("td:nth-child(6) .victory").show();
-
-        } else if (Number(this_club_result) < Number(competitor_result)) {
-            $(this).parent().find("td:nth-child(6) .defeat").show();
-        } else {
-            $(this).parent().find("td:nth-child(6) .draw").show();
-        }
-    });
-
-
-
-    // Таблица с лучшими прогнозистами 
-    $(".table_top_forecasters tbody tr:first-child td").css("border-top", "0px solid #fafafa !important");
-
-
     // Превью ко всем топ чемпионатам 
     $(".nav_ul_tour li:first-child div").addClass("active_menu");
 
@@ -151,7 +82,6 @@ $(document).ready(function() {
         $(this).children("div").addClass('active_menu');
     });
 
-    // alert(parseInt($(".nav_ul_tour li ").css('margin-left')) * 10)
 
     // Стрелки для просмотра туров которые не поместились в основной блок
 
@@ -194,6 +124,155 @@ $(document).ready(function() {
 
     $("#arrow_right_tour").bind('click', right_tour);
 
+    // Выбор недели в модальном окне
+    function prev_week() {
+        var quantity_week = $(".one_week_bet").length;
+        var height_week = $(".one_week_bet").height();
+        var margin_top = parseInt($("#all_weeks").css('margin-top'));
+        if (margin_top == 0) {
+            return;
+        } else {
+            $(this).unbind('click');
+            $("#all_weeks").animate({ 'margin-top': '+=' + height_week }, 300, function() {
+                $('.next_week_bet').bind('click', prev_week);
+            });
+        }
+    }
+
+    function next_week() {
+        var quantity_week = $(".one_week_bet").length;
+        var height_week = $(".one_week_bet").height();
+        var margin_top = parseInt($("#all_weeks").css('margin-top'));
+        if (margin_top < (-(quantity_week - 2) * height_week)) {
+            return;
+        } else {
+            $(this).unbind('click');
+            $("#all_weeks").animate({ 'margin-top': '-=' + height_week }, 300, function() {
+                $('.prev_week_bet').bind('click', next_week);
+            });
+        }
+    }
+
+    $(".next_week_bet").bind('click', prev_week);
+
+    $(".prev_week_bet").bind('click', next_week);
+
+    // Общий Коэффициент
+
+
+    var arr = [];
+    for (var i = 0; i < $(".one_user_choice").length; i++) {
+        var sum = $(".kof_this_match").eq(i).text();
+        arr.push(sum++);
+    };
+
+    function arraySum(arr) {
+        var umno = 1;
+        for (var i = 0; i < arr.length; i++) {
+            umno *= arr[i];
+        }
+        $(".total_kof_these_matches").text(umno);
+        var text_kof = +$(".total_kof_these_matches").text();
+        var text_kof = text_kof.toFixed(2);
+        $(".total_kof_these_matches").text(text_kof);
+
+    }
+    arraySum(arr);
+
+
+    // Выбор результата матча
+    $(".result_can").on("click", function() {
+        $(this).parents(".one_math_can_bet").children().removeClass("active_choice_bet");
+        $(this).addClass("active_choice_bet");
+        var match = $(this).parents(".one_math_can_bet").find(".teams_can").eq(0).find(".team_can").text() +
+            " " + "-" + " " + $(this).parents(".one_math_can_bet").find(".teams_can").eq(1).find(".team_can").text();
+        var kof_result = $(this).children(".kof_can").text();
+        // var draw_or_win = 
+        var team_won = $(this).children(".team_can").text();
+
+        if ($(this).parents(".one_math_can_bet").index() == 0) {
+            var choisce_result_thum = $(".one_user_choice").eq(0);
+        } else if ($(this).parents(".one_math_can_bet").index() == 1) {
+            var choisce_result_thum = $(".one_user_choice").eq(1);
+        } else {
+            var choisce_result_thum = $(".one_user_choice").eq(2);
+        };
+
+        if ($(this).hasClass("teams_can")) {
+            choisce_result_thum.fadeIn();
+            var win = "Победа";
+            choisce_result_thum.find(".win_or_draw").html(win);
+            choisce_result_thum.find(".your_choice_team").html(team_won);
+            choisce_result_thum.find(".kof_this_match").html(kof_result);
+            choisce_result_thum.find(".marth_choice_this").html(match);
+        } else {
+            choisce_result_thum.fadeIn();
+            var draw = "Ничья";
+            choisce_result_thum.find(".win_or_draw").html(draw);
+            var this_draw = " ";
+            choisce_result_thum.find(".your_choice_team").html(this_draw);
+            choisce_result_thum.find(".kof_this_match").html(kof_result);
+            choisce_result_thum.find(".marth_choice_this").html(match);
+        };
+
+        var arr = [];
+        for (var i = 0; i < $(".one_user_choice").length; i++) {
+            var sum = +$(".kof_this_match").eq(i).text();
+            arr.push(sum++);
+        };
+
+        console.log(arr)
+
+        function arraySum(arr) {
+            var umno = 1;
+            for (var i = 0; i < arr.length; i++) {
+                umno *= arr[i];
+            }
+            $(".total_kof_these_matches").text(umno);
+            var text_kof = +$(".total_kof_these_matches").text();
+            var text_kof = text_kof.toFixed(2);
+            $(".total_kof_these_matches").text(text_kof);
+
+        }
+        arraySum(arr);
+    });
+
+    // Удаление выбранного матча
+    $(".delete_this_this_choice i").click(function() {
+        console.log("sdadas")
+        $(this).parents(".one_user_choice").fadeOut();
+        if ($(this).parents(".one_user_choice").index() == 0) {
+            var one_result = $(".one_math_can_bet").eq(0);
+        } else if ($(this).parents(".one_user_choice").index() == 1) {
+            var one_result = $(".one_math_can_bet").eq(1);
+        } else {
+            var one_result = $(".one_math_can_bet").eq(2);
+        };
+
+        one_result.children().removeClass("active_choice_bet");
+        var kof_one = 1;
+        $(this).parents(".one_user_choice").find(".kof_this_match").html(kof_one);
+
+        var arr = [];
+        for (var i = 0; i < $(".one_user_choice").length; i++) {
+            var sum = $(".kof_this_match").eq(i).text();
+            arr.push(sum++);
+        };
+
+        function arraySum(arr) {
+            var umno = 1;
+            for (var i = 0; i < arr.length; i++) {
+                umno *= arr[i];
+            }
+            $(".total_kof_these_matches").text(umno);
+            var text_kof = +$(".total_kof_these_matches").text();
+            var text_kof = text_kof.toFixed(2);
+            $(".total_kof_these_matches").text(text_kof);
+
+        }
+        arraySum(arr);
+    });
+
     // Выбор секции в турнирной таблицы 
     $(".one_division_nav:first-child").addClass("active_tab_section");
 
@@ -202,82 +281,94 @@ $(document).ready(function() {
         $(this).addClass("active_tab_section");
     });
 
+    $(window).load(function() {
+
+        // Для картинок в меню топ матчи 
+
+        var width_nav_li = $(".li_img__top_match").width();
+
+        $(".img_top_math img").each(function(i) {
+            var img_height = $(this).height();
+            var img_width = $(this).width();
+            var new_width = (img_width / img_height) * 200;
+
+            var margin_left_li = (width_nav_li - new_width) / 2;
+            if ($(this).height() < $(this).parent().height()) {
+                $(this).css({ "height": "100%", "width": new_width, "margin-left": margin_left_li })
+            } else {
+                return;
+            }
+        });
+
+        var height = $(".colum").height();
+        var biggest_col;
+        $(".colum").each(function(i) {
+
+            if ($(this).height() < height) {
+                height = $(this).height()
+            } else {
+                biggest_col = $(this);
+            };
+
+        });
+
+        var min_height_block;
+
+        $(".colum").each(function(i) {
+
+            if ($(this).height() == height) {
+                min_height_block = $(this);
+            };
+
+        });
+
+        var width_min_height_block = $(min_height_block).width();
+
+        $(min_height_block).css("width", width_min_height_block);
+
+        var left_col = $(min_height_block).offset().left;
+
+
+
+        if (min_height_block.height() + 70 < document.documentElement.clientHeight) {
+            $(min_height_block).addClass("fixed_col_top");
+            $(min_height_block).css("bottom", (document.documentElement.clientHeight - min_height_block.height() - 70));
+            $(min_height_block).css("left", left_col);
+
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > ((document.documentElement.clientHeight - (min_height_block.height() + 50)) + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height())) {
+                    var bottom_scroll = ($(this).scrollTop() - (20 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height()));
+                    $('.fixed_col_top').css('top', 'inherit');
+                    $(min_height_block).css("bottom", bottom_scroll);
+
+
+                } else {
+                    $('.fixed_col_top').css('top', '70px');
+                    $('.fixed_col_top').css('bottom', 'inherit');
+                };
+            })
+
+        } else {
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > (45 + height - document.documentElement.clientHeight + $(".navbar").height()) && $(this).scrollTop() <= (50 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height())) {
+                    $(min_height_block).addClass("fixed_col");
+                    $(min_height_block).css("left", left_col);
+                    $(min_height_block).css("bottom", "25px")
+
+                } else if ($(this).scrollTop() > (45 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height())) {
+                    var bottom_scroll = ($(this).scrollTop() - (20 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height()));
+                    $(min_height_block).css("bottom", bottom_scroll);
+                } else {
+
+
+                    $(min_height_block).removeClass("fixed_col");
+                    $(min_height_block).css({ "left": "0px", "bottom": "0px" });
+                }
+            });
+        }
+    });
+
 
 });
 
 // Фиксация блока меньшей высоты
-
-
-
-$(window).load(function() {
-
-    var height = $(".colum").height();
-    var biggest_col;
-    $(".colum").each(function(i) {
-
-        if ($(this).height() < height) {
-            height = $(this).height()
-        } else {
-            biggest_col = $(this);
-        };
-
-    });
-
-    var min_height_block;
-
-    $(".colum").each(function(i) {
-
-        if ($(this).height() == height) {
-            min_height_block = $(this);
-        };
-
-    });
-
-    var width_min_height_block = $(min_height_block).width();
-
-    $(min_height_block).css("width", width_min_height_block);
-
-    var left_col = $(min_height_block).offset().left;
-
-
-
-    if (min_height_block.height() + 70 < document.documentElement.clientHeight) {
-        $(min_height_block).addClass("fixed_col_top");
-        $(min_height_block).css("bottom", (document.documentElement.clientHeight - min_height_block.height() - 70));
-        $(min_height_block).css("left", left_col);
-
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > ((document.documentElement.clientHeight - (min_height_block.height() + 50)) + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height())) {
-                var bottom_scroll = ($(this).scrollTop() - (20 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height()));
-                $('.fixed_col_top').css('top', 'inherit');
-                $(min_height_block).css("bottom", bottom_scroll);
-
-
-            } else {
-                $('.fixed_col_top').css('top', '70px');
-                $('.fixed_col_top').css('bottom', 'inherit');
-            };
-        })
-
-    } else {
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > (45 + height - document.documentElement.clientHeight + $(".navbar").height()) && $(this).scrollTop() <= (50 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height())) {
-                $(min_height_block).addClass("fixed_col");
-                $(min_height_block).css("left", left_col);
-                $(min_height_block).css("bottom", "25px")
-
-            } else if ($(this).scrollTop() > (45 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height())) {
-                var bottom_scroll = ($(this).scrollTop() - (20 + biggest_col.height() - document.documentElement.clientHeight + $(".navbar").height()));
-                $(min_height_block).css("bottom", bottom_scroll);
-            } else {
-
-
-                $(min_height_block).removeClass("fixed_col");
-                $(min_height_block).css({ "left": "0px", "bottom": "0px" });
-            }
-        });
-    }
-
-
-
-});
